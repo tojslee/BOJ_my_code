1870950b4adac59ed9b4bc7c2c5235417559b706
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2019-10-02 16:10:33 +0900

    11650 in C++

---
+++ 11650.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	vector<pair<int, int>> v;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		pair<int, int> p(a, b);
+		v.push_back(p);
+	}
+	sort(v.begin(), v.begin() + v.size());
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+}
